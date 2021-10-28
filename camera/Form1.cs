1eff2fd7272d5581ecb7175bde89453d04d7baec
lavahasif <lavahasif@gmail.com>
2021-10-28 22:32:57 +0530

    Add project files.

---
+++ camera/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+
+namespace camera
+{
+    public partial class Form1 : Form
+    {
+        // Create class-level accesible variables
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        bool isCameraRunning = false;
+
+        // Declare required methods
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture(0);
+            capture.Open(0);
+
+            if (capture.IsOpened())
+            {
+                while (isCameraRunning)
+                {
+                    capture.Read(frame);
+                    image = BitmapConverter.ToBitmap(frame);
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Image.Dispose();
+                    }
+
+                    pictureBox1.Image = image;
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            CaptureCamera();
+            button1.Text = "Stop";
+            isCameraRunning = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                button1.Text = "Stop";
+                isCameraRunning = true;
+            }
+            else
+            {
+                capture.Release();
+                button1.Text = "Start";
+                isCameraRunning = false;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            try
+            {
+                capture.Release();
+                camera.Abort();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isCameraRunning)
+            {
+                Bitmap snapshot = new Bitmap(pictureBox1.Image);
+
+
+                snapshot.Save(string.Format(@"E:\image.jpg", Guid.NewGuid()), ImageFormat.Jpeg);
+                capture.Release();
+                camera.Abort();
+                this.Close();
+            }
+            else
+            {
+                Console.WriteLine("Cannot take picture if the camera isn't capturing image!");
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+        }
+    }
+}
